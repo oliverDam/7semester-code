30811394dee6ddb462f1bdc38a9be730e602a522
Phthalaten <sbr@familienbruun.dk>
2017-10-06 11:10:35 +0200

    for-looped the acceleration analysis

--- Data/AccelAnalysis.m
+++ Data/AccelAnalysis.m
@@ -2,23 +2,17 @@ function AccelAnalysis(accelData)
 
 %Splits into different columns and removes nAn
 
-accelData1 = accelData(:,[1]);
-accelData2 = accelData(:,[2]);
-accelData3 = accelData(:,[3]);
-
-accelData1(isnan(accelData1)) = [];
-accelData2(isnan(accelData2)) = [];
-accelData3(isnan(accelData3)) = [];
-
-accelData1 = transpose(accelData1);
-accelData2 = transpose(accelData2);
-accelData3 = transpose(accelData3);
+for i=1:3
+    accel = accelData(:,[i]);
+    accel(isnan(accel)) = [];
+    accelMat{i} = transpose(accel);
+end
 
 %% Plotting the x, y and z axis of the gyroscope
 
 figure;
 hold on;
-plot(accelData1,'m');
-plot(accelData2,'g');
-plot(accelData3,'b');
+for i=1:3
+    plot(accelMat{i});
+end
 hold off;
