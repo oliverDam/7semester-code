2936e2de5bddc7b8342ca2cd5f4c182956416e94
Phthalaten <sbr@familienbruun.dk>
2017-10-06 11:03:40 +0200

    Renamed some stuff so it makes sense

--- Data/removeAndSplit.m
+++ Data/removeAndSplit.m
@@ -3,12 +3,12 @@
 function [output] = removeAndSplit(emgData)
 
 for i=1:8
-    pancake = emgData(:,[i]);
-    pancake(isnan(pancake)) = [];
-    cakepan{i} = transpose(pancake);
+    column = emgData(:,[i]);
+    column(isnan(column)) = [];
+    columnMatrix{i} = transpose(column);
 end
 
-output = [cakepan{1}; cakepan{2}; cakepan{3}; cakepan{4}; cakepan{5}; ...
-    cakepan{6}; cakepan{7}; cakepan{8}];
+output = [columnMatrix{1}; columnMatrix{2}; columnMatrix{3}; columnMatrix{4}; columnMatrix{5}; ...
+    columnMatrix{6}; columnMatrix{7}; columnMatrix{8}];
 
 end
