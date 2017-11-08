d682299b4cb63f46e0008be794ff143f616c5ac0
Phthalaten <sbr@familienbruun.dk>
2017-11-08 09:15:43 +0100

    Wrote a lot of comments

--- MyoMex-master/MyoMex/functions/findMVC.m
+++ MyoMex-master/MyoMex/functions/findMVC.m
@@ -2,57 +2,73 @@
 % "Plot Button" is pressed
 
 function trapezoidPlot(handles, m1)
-        
-    
-    % gets the value from the slider (value is 1 to 10)
-    pause(0.1);
-    x = [0 2000 4000 8000 9000 10000];
-    y = [0.01, 0.01, 0.9, 0.9, 0.01 0.01];
-    plothandle = handles;
-    if ~isempty(plothandle);
-        cla();
-        hold on;
-        axes(plothandle);
-        plot(x,y);
-        
-        %Gets the time we want to record:
-        %recordingTime = str2double(get(handles.edit1))*1000;
-        recordingTime = 10;
-        buffer = 0;
-        time = 0;
-        lol = plot(0,0);
-        windowSize = 40;
-        MVC = 0;
-        
-        
-            while time <= recordingTime
-                
-                timeEMG = m1.timeEMG_log;
-                    if ~isempty(timeEMG)
-                        iiEMG = find(timeEMG>=(timeEMG(end)-2));
-                        tEMG = timeEMG(iiEMG);
-                        dataMatrix(iiEMG,:) = m1.emg_log(iiEMG,:);
-                        lastSample = max(iiEMG);
-                        max(dataMatrix(iiEMG,:))
-                        if lastSample >= windowSize && buffer >= windowSize
-                            time = m1.timeEMG;
-                            toBeFiltered = dataMatrix(lastSample-(windowSize-1):lastSample,1:8);
-                            filterEmg = butterFilter(toBeFiltered);
-                            meanAbs = mean(abs(filterEmg));
-                            maxEmg = max(meanAbs);
-                            MVC(lastSample) = maxEmg;
-                            axes(plothandle);
-                            delete(lol);
-                            lol = plot(time*1000, maxEmg, 'or', 'MarkerSize', 5, 'MarkerFaceColor', 'g');
-                            drawnow;
-                            buffer = 0;
-                        else
-                            buffer = buffer + 1;
-                        end
-                    end
 
-                end
+%Pause for initialization or something:
+pause(0.1);
+    
+%Setup of the trapezoid:
+x = [0 2000 4000 8000 9000 10000];
+y = [0.01, 0.01, 0.9, 0.9, 0.01 0.01];
+
+%Required setup. Do NOT touch unless you want to screw every single thing
+%in the entire program up!
+plothandle = handles;
+if ~isempty(plothandle);
+    cla();
+    hold on;
+    axes(plothandle);
+    plot(x,y);
+        
+    %A little more of the setup. You're not allowed to touch this either!
+    recordingTime = 10;
+    buffer = 0;
+    time = 0;
+    lol = plot(0,0);
+    windowSize = 40;
+    MVC = 0;
+    
+    %Makes sure we're recording for 'recordingTime' seconds:
+    while time <= recordingTime
+        
+        %The following lines collects the data (code stolen from MyoMex)
+        timeEMG = m1.timeEMG_log;
+        if ~isempty(timeEMG)
+            iiEMG = find(timeEMG>=(timeEMG(end)-2));
+            tEMG = timeEMG(iiEMG);
+            dataMatrix(iiEMG,:) = m1.emg_log(iiEMG,:);
+            lastSample = max(iiEMG);
+            max(dataMatrix(iiEMG,:))
+            
+            %Creates a window with the size 'windowSize', in order to
+            %split it into something which we can filter in real time. This
+            %doesn't provide any overlap in the data.
+            if lastSample >= windowSize && buffer >= windowSize
+                time = m1.timeEMG;
+                toBeFiltered = dataMatrix(lastSample-(windowSize-1):lastSample,1:8);
+                filterEmg = butterFilter(toBeFiltered);
+                
+                %I'm not completely sure about the following lines, since
+                %i was a bit too tired when i wrote them. Figure it out
+                %guys!
+                meanAbs = mean(abs(filterEmg));
+                maxEmg = max(meanAbs);
+                MVC(lastSample) = maxEmg;
+                
+                %This plots the dot, and it works as it should
+                axes(plothandle);
+                delete(lol);
+                lol = plot(time*1000, maxEmg, 'or', 'MarkerSize', 5, 'MarkerFaceColor', 'g');
+                drawnow;
+                buffer = 0;
+            else
+                buffer = buffer + 1;
+            end
+        end
     end
-    MVC = max(MVC);
-    save('MVC.mat','MVC');
+end
+
+%Finds the max value in the MVC and saves it
+MVC = max(MVC);
+save('MVC.mat','MVC');
+
 end
--- MyoMex-master/MyoMex/functions/trapezoidPlot.m
+++ MyoMex-master/MyoMex/functions/trapezoidPlot.m
@@ -2,19 +2,22 @@
 % "Plot Button" is pressed
 
 function trapezoidPlot(sliderValue, handles, m1)
-    try
-        load('MVC.mat');
-        maximum = max(MVC);
-        disp('MVC found');
-        isError = 0;
-    catch
-        warning('MVC not found - go record it before trying again');
-        isError = 1;
-    end
-    
-    if isError ~= 1
-    % gets the value from the slider (value is 1 to 10)
+%Tries to load the MVC we've recorded:
+try
+    load('MVC.mat');
+    maximum = max(MVC);
+    disp('MVC found');
+    isError = 0;
+catch
+    warning('MVC not found - go record it before trying again');
+    isError = 1;
+end
+
+%Will only do the following if there's no error
+if isError ~= 1
     pause(0.1);
+    
+    %Creates the trapezoid based on the slidervalue from the input.
     x = [0 2000 4000 8000 9000 10000];
     y = [0.01, 0.01, sliderValue, sliderValue, 0.01 0.01];
     plothandle = handles;
@@ -24,48 +27,61 @@ function trapezoidPlot(sliderValue, handles, m1)
         axes(plothandle);
         plot(x,y);
         
-        %Gets the time we want to record:
-        %recordingTime = str2double(get(handles.edit1))*1000;
+        %Setup for later use. Do NOT change it unless you want to fix it
+        %after you screw it up.
         recordingTime = 10;
         buffer = 0;
         time = 0;
         lol = plot(0,0);
         windowSize = 40;
-        maxEmg = 0;
         
-        
-            while time <= recordingTime
+        %Makes sure we'll record for the stated 'recordingTime'
+        while time <= recordingTime
+            
+            %This has been stolen from MyoMex to retrieve data:
+            timeEMG = m1.timeEMG_log;
+            if ~isempty(timeEMG)
+                iiEMG = find(timeEMG>=(timeEMG(end)-2));
+                tEMG = timeEMG(iiEMG);
+                dataMatrix(iiEMG,:) = m1.emg_log(iiEMG,:);
                 
-                timeEMG = m1.timeEMG_log;
-                    if ~isempty(timeEMG)
-                        iiEMG = find(timeEMG>=(timeEMG(end)-2));
-                        tEMG = timeEMG(iiEMG);
-                        dataMatrix(iiEMG,:) = m1.emg_log(iiEMG,:);
-                        lastSample = max(iiEMG);
-                        %maxEmg = maxEmg + max(dataMatrix(iiEMG,:));
-                        if lastSample >= windowSize && buffer >= windowSize
-                            time = m1.timeEMG;
-                            toBeFiltered = dataMatrix(lastSample-(windowSize-1):lastSample,1:8);
-                            filterEmg = butterFilter(toBeFiltered);
-                            meanAbs = mean(abs(filterEmg));
-                            maxEmg = max(meanAbs);
-                            maxEmg = max(rescale(maxEmg, 'InputMin', 0, 'InputMax', maximum));
-                            axes(plothandle);
-                            delete(lol);
-                            lol = plot(time*1000, maxEmg, 'or', 'MarkerSize', 5, 'MarkerFaceColor', 'g');
-                            drawnow;
-                            buffer = 0;
-                        else
-                            buffer = buffer + 1;
-                        end
-                    end
-
+                %Makes sure we get the stated 'windowSize' of the buffer
+                lastSample = max(iiEMG);
+                if lastSample >= windowSize && buffer >= windowSize
+                    
+                    %Gets the time we've recorded EMG in this function
+                    time = m1.timeEMG;
+                    
+                    %Finds and filters the window we've selected
+                    toBeFiltered = dataMatrix(lastSample-(windowSize-1):...
+                        lastSample,1:8);
+                    filterEmg = butterFilter(toBeFiltered);
+                    
+                    %This I'm not so sure about:
+                    meanAbs = mean(abs(filterEmg));
+                    maxEmg = max(meanAbs);
+                    
+                    %Rescales the input between 0 and our MVC
+                    maxEmg = max(rescale(maxEmg, 'InputMin', 0, ...
+                        'InputMax', maximum));
+                    
+                    %Plots the dot:
+                    axes(plothandle);
+                    delete(lol);
+                    lol = plot(time*1000, maxEmg, 'or', 'MarkerSize', ...
+                        5, 'MarkerFaceColor', 'g');
+                    drawnow;
+                    buffer = 0;
+                else
+                    buffer = buffer + 1;
                 end
+            end
+        end
     end
     
-    
-    %This should be changed in order to save it as a specific file (might
+    %This could be changed in order to save it as a specific file (might
     %be able to do it with a textfield in the GUI before testing?):
+    dataMatrix = butterFilter(dataMatrix);
     save('testData.mat','dataMatrix');
-    end
+end
 end
