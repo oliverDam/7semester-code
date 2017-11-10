8be20576e4e122d2528c5b6a66149c30e51c8b4a
Phthalaten <sbr@familienbruun.dk>
2017-11-10 14:00:37 +0100

    wollo ma fiend

--- MyoMex-master/MyoMex/removeBaseline.m
+++ MyoMex-master/MyoMex/removeBaseline.m
@@ -1,6 +1,7 @@
 function output = removeBaseline(inputData, baseline)
 
-%Tried to remove the baseline:
+%Tried to remove the baseline by dividing the sample by 5 if it's below the
+%recorded baseline for each channel.
 
 for i=1:8
     if abs(baseline(i)) >= abs(inputData(i))
