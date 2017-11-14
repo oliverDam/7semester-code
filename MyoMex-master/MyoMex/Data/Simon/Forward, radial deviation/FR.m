6d5d56521253afdd068ebcf07d1ed2fb378c3fec
Phthalaten <sbr@familienbruun.dk>
2017-11-14 14:49:15 +0100

    Fixed some of the files..

---
+++ MyoMex-master/MyoMex/Data/Simon/Forward, radial deviation/FR.m
@@ -0,0 +1,35 @@
+load('AccelData30')
+
+FR_AccelData30 = accelMatrix;
+
+save('FR_AccelData30.mat','FR_AccelData30')
+
+load('AccelData50')
+
+FR_AccelData50 = accelMatrix;
+
+save('FR_AccelData50.mat','FR_AccelData50')
+
+load('AccelData80')
+
+FR_AccelData80 = accelMatrix;
+
+save('FR_AccelData80.mat','FR_AccelData80')
+
+load('EmgData30')
+
+FR_EmgData30 = EmgMatrix;
+
+save('FR_EmgData30.mat','FR_EmgData30')
+
+load('EmgData50')
+
+FR_EmgData50 = EmgMatrix;
+
+save('FR_EmgData50.mat','FR_EmgData50')
+
+load('EmgData80')
+
+FR_EmgData80 = EmgMatrix;
+
+save('FR_EmgData80.mat','FR_EmgData80')
