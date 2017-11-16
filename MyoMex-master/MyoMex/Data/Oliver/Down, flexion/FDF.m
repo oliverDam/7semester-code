c87162923a0ca1db3e5aa21df3494c85b7c2454c
Phthalaten <sbr@familienbruun.dk>
2017-11-16 09:39:12 +0100

    Corrected the data

--- MyoMex-master/MyoMex/Data/Oliver/Down, flexion/FDF.m
+++ MyoMex-master/MyoMex/Data/Oliver/Down, flexion/FDF.m
@@ -1,18 +1,18 @@
 
 load('DF_EmgData30.mat')
 
-FDF_EmgData30 = featureExtractionMovVar(DE_EmgData30(801:1400,:));
+FDF_EmgData30 = featureExtractionMovVar(DF_EmgData30(801:1400,:));
 
 save('FDF_EmgData30.mat','FDF_EmgData30')
 
 load('DF_EmgData50.mat')
 
-FDF_EmgData50 = featureExtractionMovVar(DE_EmgData80(801:1400,:));
+FDF_EmgData50 = featureExtractionMovVar(DF_EmgData80(801:1400,:));
 
 save('FDF_EmgData50.mat','FDF_EmgData50')
 
 load('DF_EmgData80.mat')
 
-FDF_EmgData80 = featureExtractionMovVar(DE_EmgData80(801:1400,:));
+FDF_EmgData80 = featureExtractionMovVar(DF_EmgData80(801:1400,:));
 
 save('FDF_EmgData80.mat','FDF_EmgData80')
