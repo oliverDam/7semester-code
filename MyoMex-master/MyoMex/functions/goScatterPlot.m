6f7a1a1bb38f5cabbdf63ef49a68eb6b46319975
MartinGarenfeld <31693969+MartinGarenfeld@users.noreply.github.com>
2017-11-23 10:23:21 +0100

    ver 2

--- MyoMex-master/MyoMex/functions/goScatterPlot.m
+++ MyoMex-master/MyoMex/functions/goScatterPlot.m
@@ -10,6 +10,17 @@ overallMovement = [Ex;Fl;Ra;Ul];
 
 [pcs, scrs, ~, ~, pexp] = pca(bigMatrix);
 
+figure(1)
+%Intensity of principal components
 pareto(pexp);
+title('PCA of the MAV feature of the four movements')
 
-scatter3(scrs( :, 1 ), scrs( :, 2 ), scrs( :, 3 ), 10, overallMovement)
+%Scatterplot of the four movements
+figure(2)
+scatter3(scrs(1:270, 1 ), scrs(1:270, 2 ), scrs(1:270, 3 ), 10, overallMovement(1:270,1),'filled')
+hold on
+scatter3(scrs(271:540, 1 ), scrs(271:540, 2 ), scrs(271:540, 3 ), 10, overallMovement(271:540,1),'filled')
+scatter3(scrs(541:810, 1 ), scrs(541:810, 2 ), scrs(541:810, 3 ), 10, overallMovement(541:810,1),'filled')
+scatter3(scrs(811:1080, 1 ), scrs(811:1080, 2 ), scrs(811:1080, 3 ), 10, overallMovement(811:1080,1),'filled')
+legend('Extension','Flexion','Radial deviation','Ulnar deviation')
+title('Scatter plot of PCA of the MAV feature for the four movements')
