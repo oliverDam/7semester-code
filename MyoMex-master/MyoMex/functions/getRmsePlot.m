16827b4c7aedb81c7917eba5bed4745ed412a208
Phthalaten <sbr@familienbruun.dk>
2017-11-24 12:13:18 +0100

    Fixed getRmsestuff

--- MyoMex-master/MyoMex/functions/getRmsePlot.m
+++ MyoMex-master/MyoMex/functions/getRmsePlot.m
@@ -3,19 +3,24 @@ function getRmsePlot(RMSEMav,RMSELogVar)
 std_Mav = std(RMSEMav);
 std_Log = std(RMSELogVar);
 
+stdOverall_Mav = std(RMSEMav(:));
+stdOverall_Log = std(RMSELogVar(:));
+
 mean_Mav = mean(RMSEMav);
 mean_Log = mean(RMSELogVar);
 
-std_matrix = [std_Mav, std_Log]
+meanOverall_Mav = mean(RMSEMav(:));
+meanOverall_Log = mean(RMSELogVar(:));
 
-mean_matrix = [mean_Mav, mean_Log]
+std_matrix = [std_Mav, stdOverall_Mav, std_Log, stdOverall_Log];
+
+mean_matrix = [mean_Mav, meanOverall_Mav, mean_Log, meanOverall_Log]
 
 
 figure;
 hold on;
-bar(1:8,mean_matrix);
-errorbar(1:8,mean_matrix,std_matrix,'.');
-somenames = {'','Extension MAV','Flexion MAV','Radial MAV','Ulnar MAV', ...
-    'Extension LogVar','Flexion LogVar','Radial LogVar','Ulnar LogVar','',''};
+bar(1:10,mean_matrix);
+errorbar(1:10,mean_matrix,std_matrix,'.');
+somenames = {'','Extension MAV','Flexion MAV','Radial MAV','Ulnar MAV','Overall MAV', 'Extension LogVar','Flexion LogVar','Radial LogVar','Ulnar LogVar','Overall LogVar',''};
 set(gca,'xticklabel',somenames);
 ylabel('Error')
